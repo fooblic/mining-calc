8b870e97b6fdbf1cae60ad478e10c07f7b28864a
fooblic <fooblic@users.noreply.github.com>
2017-07-13 23:58:30 +0200

    cloud mine compare

--- mine2.py
+++ mine2.py
@@ -11,69 +11,28 @@ import pylab as py
 import requests
 import yaml
 
+import pandas as pd
+
 TODAY = time.strftime("%y%m%d")
 
-CFG = yaml.load(open("investing.yml.example"))
+CFG = yaml.load(open("investing.yml"))
+
+CURRENCY = CFG["Coins"].keys()  #["BTC", "LTC", "ETH", "DASH", "ZEC"]
+MINING = CFG["Mining"].keys()  #["HF", "GM"]
+qty = len(CURRENCY) * len(MINING)
 
 URL_BASE = "https://api.coinmarketcap.com/v1/ticker/"
 records = {"BTC": 
-               {"API": URL_BASE + "bitcoin",
-                "price": 0,
-                "mining": # https://alloscomp.com/bitcoin/calculator
-                    {
-                    "MINE" : 1.2,          # USD per 10GH/s
-                    "MAINT" : 0.0035,      # USD fee per 10GH/s per day
-                    "COINS" : 353 * 1e-8,  # satoshi per day for 10GH/s for current difficulty
-                    "IN": CFG['BTC']       # USD investing
-                    }
-                },
+               {"API": URL_BASE + "bitcoin"},
             "LTC": 
-                {"API": URL_BASE + "litecoin",
-                 "price": 0,
-                 "mining": # http://www.coinwarz.com/calculators/litecoin-mining-calculator/
-                     {
-                     "MINE" : 13.5,     # USD per 1 MH/s
-                     "MAINT" : 0.01,    # USD fee per 1 MH/s per day
-                     "COINS" : 0.002067,  # coins per day
-                     "IN": CFG['LTC']     # USD investing
-                     }
-                },
+                {"API": URL_BASE + "litecoin"},
             "ETH": 
-                {"API": URL_BASE + "ethereum",
-                 "price": 0,
-                 "mining": # https://etherscan.io/ether-mining-calculator
-                     {
-                     "MINE" : 2.2,      # USD per 100 KH/s
-                     "MAINT" : 0,
-                     "COINS" : 4.3849 * 1e-5, # coins per day
-                     "IN": CFG['ETH']        # USD investing
-                     }
-                 },
+                {"API": URL_BASE + "ethereum"},
             "DASH": 
-                {"API": URL_BASE + "dash",
-                 "price": 0,
-                 "mining": # http://www.coinwarz.com/calculators/litecoin-mining-calculator/
-                     {
-                     "MINE" : 5.8,      # USD per 1 MH/s
-                     "MAINT" : 0,
-                     "COINS" : 0.00013255, # coins per day
-                     "IN": CFG['DASH']        # USD investing
-                     }
-                },
+                {"API": URL_BASE + "dash"},
             "ZEC": 
-                {"API": URL_BASE + "zcash",
-                 "price": 0,
-                 "mining": # https://www.coinwarz.com/calculators/zcash-mining-calculator/
-                     {
-                     "MINE" : 2.0,      # USD per 0.1 H/s
-                     "MAINT" : 0,
-                     "COINS" : 0.00000257, # coins per day
-                     "IN": CFG['ZEC']        # USD investing
-                     }
-                }
+                {"API": URL_BASE + "zcash"}
 }
-
-CURRENCY = ["BTC", "LTC", "ETH", "DASH", "ZEC"]
 
 print("Getting ex-rates...\n")
 # BTC/USD rate
@@ -84,8 +43,19 @@ if rep.status_code == 200:
 # Ex-rates in BTC
 for cur in CURRENCY:
     rep = requests.get(records[cur]["API"])
+    records[cur].update(price=0)
     if rep.status_code == 200:
         records[cur]["price"] = float(rep.json()[0]["price_btc"])
+
+# Get mining data from config
+for cur in CURRENCY:
+    for mine in MINING:
+        records[cur].update(mine={})
+        miner = {
+             "MINE" : CFG["Mining"][mine][cur]["mine"],
+             "MAINT" : CFG["Mining"][mine][cur]["maint"]
+        }
+        records[cur][mine] = miner
 
 reporting = "BTC_USD: %s USD\n" % BTC_USD
 for cur in CURRENCY:
@@ -106,51 +76,75 @@ def income(ddays, coins, exrate, unit):
     ''' Revenue '''
     return BTC_USD * (coins * unit * exrate) * ddays
 
-os.mkdir("img" + TODAY)
+try:
+    os.mkdir("img" + TODAY)
+except:
+    print("Could not create %s" % ("img" + TODAY))
+
 num = 0
 DAYS = 365 * 1
+col = ["currency", "mining", "invest", "revenue", "maintance", "profit",
+               "return", "percent"]
+table = pd.DataFrame([], columns=col, index=range(qty))
 
 for cur in CURRENCY:
-    print("Calculating %s..." % cur)
-    data = records[cur]["mining"]
-    units = data["IN"] / data["MINE"]
-    fee = data["MAINT"] * units  # per day
+    for mine in MINING:
+        print("Calculating %s %s..." % (cur, mine))
 
-    profit = []
-    inc = []
-    out = []
+        table.loc[num]["currency"] = cur
+        table.loc[num]["mining"] = mine
 
-    reporting += "\n" + cur
-    for day in range(DAYS):
-        out.append(outcome(day, fee, data["IN"]))
-        inc.append(income(day, data["COINS"], records[cur]["price"], units))
-        profit.append(inc[day] - out[day])
+        data = records[cur][mine]
+        investing = CFG["Investing"][cur]
+        table.loc[num]["invest"] = investing
+        units = investing / data["MINE"]
+        fee = data["MAINT"] * units  # per day
+        print("Units %.1f / Fee %s" % (units, fee))
 
-        if (profit[day-1] < 0) and (profit[day] >= 0):
-            reporting += " return in " + str(day) + " days"
+        profit = []
+        inc = []
+        out = []
 
-        if day == (DAYS - 1):
-            pro = "%.1f" % (profit[day] / data["IN"] * 100)
-            reporting += TEMPL % (data["IN"], day,
-                                  inc[day],
-                                  out[day] - data["IN"],
-                                  profit[day],
-                                  profit[day] / data["IN"] * 100
-                                 )
+        reporting += "\n %s %s" % (cur, mine) 
+        for day in range(DAYS):
+            out.append(outcome(day, fee, investing))
+            inc.append(income(day, CFG["Coins"][cur], records[cur]["price"], units))
+            profit.append(inc[day] - out[day])
 
-    py.figure(num)
-    py.title(cur + " -> " + pro + "% per year")
-    py.xlabel("days")
-    py.plot(out, label="outcome")
-    py.plot(inc, label="income")
-    py.plot(profit, label="profit")
-    py.legend(loc="best")
-    #py.show()
-    py.savefig("./img" + TODAY +"/" + cur + ".png")
-    num += 1
+            if (profit[day-1] < 0) and (profit[day] >= 0):
+                roi = day/30
+                reporting += " return in %.1f monthes" % roi
+                table.loc[num]["return"] = roi
+
+            if day == (DAYS - 1):
+                calc = ""
+                pro = "%.1f" % (profit[day] / investing  * 100)
+                calc = TEMPL % (investing, day,
+                                inc[day],
+                                out[day] - investing,
+                                profit[day],
+                                profit[day] / investing * 100
+                                )
+                #print(calc)
+                reporting += calc
+                table.loc[num]["revenue"] = inc[day]
+                table.loc[num]["maintance"] = out[day] - investing
+                table.loc[num]["profit"] = profit[day]
+                table.loc[num]["percent"] = profit[day] / investing * 100
+
+        py.figure(num)
+        py.title(cur + "_" + mine + " -> " + pro + "% per year")
+        py.xlabel("days")
+        py.plot(out, label="outcome")
+        py.plot(inc, label="income")
+        py.plot(profit, label="profit")
+        py.legend(loc="best")
+        #py.show()
+        py.savefig("./img" + TODAY +"/" + cur + "_" + mine + ".png")
+        num += 1
 
 with open("report" + TODAY + ".txt", "w") as FILE:
     FILE.write(reporting)
 
-print(reporting)
-
+#print(reporting)
+print(table.sort(["percent"]))
