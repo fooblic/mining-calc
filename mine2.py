e9b1ee547dbcfa224f21dd711061a5b920889023
fooblic <fooblic@users.noreply.github.com>
2017-12-10 21:26:28 +0100

    CPE mining comparison

--- mine2.py
+++ mine2.py
@@ -23,29 +23,53 @@ pp = pprint.PrettyPrinter(indent=4)
 TODAY = time.strftime("%y%m%d")
 TD = time.strftime("%b %d, %Y")
 
-CFG = yaml.load(open("test3.yml"))
-#headers = ["Currency", "Coins"]
-#print(tabulate([[c,v] for c,v in CFG["Coins"].items()], headers=headers))
+CFG = yaml.load(open("mine.yml"))
 pp.pprint(CFG["Coins"])
 
 CURRENCY = CFG["Coins"].keys()
-MINING = CFG["Mining"].keys()
-qty = len(CURRENCY) * len(MINING)
+CLOUD = CFG["Cloud"].keys()
+
+# CPE mining data
+GEARS = CFG["CPE"].keys()
+HASH = CFG["HASH"] 
+
+MINING = list(CLOUD) + list(GEARS)
+CPE_NUM = len(GEARS) * len(HASH)
 
 reporting = TD + "\n"
-for mine in MINING:
-    #reporting += "\n%s\n %s" % (mine, CFG["Mining"][mine])
+for mine in CLOUD:
     reporting += "\n%s\n" % mine
-    reporting += pprint.pformat(CFG["Mining"][mine], indent=4)
+    reporting += pprint.pformat(CFG["Cloud"][mine], indent=4)
 
-URL_BASE = "https://api.coinmarketcap.com/v1/ticker/"
-records = {"BTC": {"API": URL_BASE + "bitcoin"},
-           "LTC": {"API": URL_BASE + "litecoin"},
-           "ETH": {"API": URL_BASE + "ethereum"},
-           "DASH":{"API": URL_BASE + "dash"},
-           "ZEC": {"API": URL_BASE + "zcash"},
-           "XMR": {"API": URL_BASE + "monero"}
-          }
+cpe = pd.DataFrame([],
+                    columns=["cpe", "cost", "alg", "rate", "units", "power", "mine", "maint"],
+                    index=arange(CPE_NUM))
+numb = 0
+for item in GEARS:
+    for alg in HASH:
+        try:
+            cpe.loc[numb]["cpe"] = item
+            cpe.loc[numb]["cost"] = CFG["CPE"][item]["Cost"] / CFG["USD_CUR"]
+            cpe.loc[numb]["alg"] = alg
+            cpe.loc[numb]["rate"] = CFG["CPE"][item]["Hash"][alg]["Rate"]
+            cpe.loc[numb]["units"] = CFG["Units"][alg]
+            cpe.loc[numb]["power"] = CFG["CPE"][item]["Hash"][alg]["Power"]
+            cpe.loc[numb]["mine"] = cpe.loc[numb]["cost"] / cpe.loc[numb]["rate"]
+            cpe.loc[numb]["maint"] = CFG["KWH"] / CFG["USD_CUR"] * cpe.loc[numb]["power"] / 1000 * 24 / cpe.loc[numb]["rate"]
+        except KeyError:
+            #print("Error key: %s %s" % (item, alg))
+            pass
+        numb += 1
+
+cpe = cpe[cpe["maint"] > 0]
+cpe_report = tabulate(cpe, headers="keys", tablefmt="orgtbl", showindex=False)
+reporting += "\n" + cpe_report + "\n"
+print(cpe_report)
+
+URL_BASE = CFG["URL_BASE"]
+records = dict.fromkeys(CURRENCY)
+for cur in CURRENCY:
+    records[cur] = dict({"API": URL_BASE + CFG["API"][cur]} )
 
 print("Getting ex-rates...\n")
 # BTC/USD rate
@@ -59,21 +83,34 @@ for cur in CURRENCY:
     records[cur].update(price=0)
     if rep.status_code == 200:
         records[cur]["price"] = float(rep.json()[0]["price_btc"])
+#pp.pprint(records)
 
 INDX = []  # index for pandas df
 # Get mining data from config
 for cur in CURRENCY:
     for mine in MINING:
         records[cur].update(mine={})
-        try:
-            miner = {
-                "MINE": CFG["Mining"][mine][cur]["mine"],
-                "MAINT": CFG["Mining"][mine][cur]["maint"]
-            }
-            INDX.append("%s-%s" % (cur, mine))
-            records[cur][mine] = miner
-        except KeyError:
-            print("error: ", cur, mine)
+        if mine in GEARS:
+            try:
+                cpe_table = cpe[cpe["cpe"] == mine]
+                miner = {
+                    "MINE": cpe_table[cpe_table["alg"] == CFG["ALG"][cur]]["mine"].item(),
+                    "MAINT": cpe_table[cpe_table["alg"] == CFG["ALG"][cur]]["maint"].item()}
+                INDX.append("%s-%s" % (cur, mine))
+                records[cur][mine] = miner
+            except ValueError:
+                #print("error: ", cur, mine)
+                pass
+        else:
+            try:
+                miner = {
+                    "MINE": CFG["Cloud"][mine][cur]["mine"],
+                    "MAINT": CFG["Cloud"][mine][cur]["maint"]}
+                INDX.append("%s-%s" % (cur, mine))
+                records[cur][mine] = miner
+            except KeyError:
+                #print("error: ", cur, mine)
+                pass
 
 # Get mining dataframe from config
 mining = pd.DataFrame([],
@@ -83,13 +120,19 @@ mining = pd.DataFrame([],
 for cur in CURRENCY:
     for mine in MINING:
         try:
-            mining.loc["%s-%s" % (cur, mine)]["rate"] = CFG["Units"][cur]
+            mining.loc["%s-%s" % (cur, mine)]["rate"] = CFG["Units"][CFG["ALG"][cur]]
             mining.loc["%s-%s" % (cur, mine)]["coins"] = CFG["Coins"][cur]
             mining.loc["%s-%s" % (cur, mine)]["price"] = records[cur]["price"]
-            mining.loc["%s-%s" % (cur, mine)]["mine"] = CFG["Mining"][mine][cur]["mine"]
-            mining.loc["%s-%s" % (cur, mine)]["maint"] = CFG["Mining"][mine][cur]["maint"]
+            if mine in GEARS:
+                cpe_table = cpe[cpe["cpe"] == mine]
+                mining.loc["%s-%s" % (cur, mine)]["mine"] = cpe_table[cpe_table["alg"] == CFG["ALG"][cur]]["mine"].item()
+                mining.loc["%s-%s" % (cur, mine)]["maint"] = cpe_table[cpe_table["alg"] == CFG["ALG"][cur]]["maint"].item()
+            else:
+                mining.loc["%s-%s" % (cur, mine)]["mine"] = CFG["Cloud"][mine][cur]["mine"]
+                mining.loc["%s-%s" % (cur, mine)]["maint"] = CFG["Cloud"][mine][cur]["maint"]
         except KeyError:
-            print("error: ", cur, mine)
+            #print("error: ", cur, mine)
+            pass
 
 mine_report = tabulate(mining, headers="keys", tablefmt="orgtbl", showindex=True)
 reporting += "\n" + mine_report + "\n"
@@ -127,17 +170,17 @@ COL = ["invest", "fee", "earn", "ratio",
 table = pd.DataFrame([], columns=COL, index=INDX)
 
 for cur in CURRENCY:
+    cur_price = records[cur]["price"]
     for mine in MINING:
 
         num = cur + "-" + mine
         if num in INDX:
 
-            data = records[cur][mine]
             investing = CFG["Investing"][cur]
             table.loc[num]["invest"] = investing
-            units = investing / data["MINE"]
-            fee = data["MAINT"] * units  # per day USD
-            earn = CFG["Coins"][cur] * units * records[cur]["price"] * BTC_USD  # USD
+            units = investing / mining.loc[num]["mine"]
+            fee = mining.loc[num]["maint"] * units  # per day USD
+            earn = CFG["Coins"][cur] * units * cur_price * BTC_USD  # USD
             ratio = (earn - fee) / CFG["Investing"][cur] * 100  # profit per day per investment %
 
             table.loc[num]["fee"] = fee
@@ -151,7 +194,7 @@ for cur in CURRENCY:
             reporting += "\n%s:" % num
             for day in range(DAYS):
                 out.append(outcome(day, fee, investing))
-                inc.append(income(day, CFG["Coins"][cur], records[cur]["price"], units))
+                inc.append(income(day, CFG["Coins"][cur], cur_price, units))
                 profit.append(inc[day] - out[day])
 
                 if (profit[day-1] < 0) and (profit[day] >= 0):  # point of investment return
@@ -197,8 +240,8 @@ print(table)
 
 # Graph
 plt.rcdefaults()
-mpl.rcParams["font.family"] = "serif"
-mpl.rcParams["font.size"] = 14
+#mpl.rcParams["font.family"] = "serif"
+#mpl.rcParams["font.size"] = 14
 fig, ax = plt.subplots()
 y_pos = arange(len(table.index))
 ax.barh(y_pos - 0.5, table["percent"])
@@ -206,8 +249,8 @@ ax.set_yticks(y_pos)
 ax.set_yticklabels(table.index.values)
 ax.set_xlabel('Percents per year, %')
 ax.invert_yaxis()
-plt.grid()
-#plt.autoscale(tight=True)
+plt.autoscale(tight=True)
 plt.subplots_adjust(left=0.22)
+plt.grid()
 plt.savefig("./report~/percent-%s.png" % TODAY)
 #plt.show()
